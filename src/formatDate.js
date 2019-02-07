61234130b9f8a490aa8df7c099429b6f8410ce02
Mike Bostock <mbostock@gmail.com>
2019-02-07 12:58:33 -0800

    Fix #39 date-only format.

--- src/formatDate.js
+++ src/formatDate.js
@@ -3,22 +3,21 @@ function pad(value, width) {
   return length < width ? new Array(width - length + 1).join(0) + s : s;
 }
 
+function isUTCMidnight(date) {
+  return date.getUTCMilliseconds() === 0
+      && date.getUTCSeconds() === 0
+      && date.getUTCMinutes() === 0
+      && date.getUTCHours() === 0;
+}
+
 export default function formatDate(date) {
-  return isNaN(date) ? "Invalid Date"
-      : pad(date.getFullYear(), 4) + "-"
-      + pad(date.getMonth() + 1, 2) + "-"
-      + pad(date.getDate(), 2)
-      + (date.getMilliseconds() ? "T"
-          + pad(date.getHours(), 2) + ":"
-          + pad(date.getMinutes(), 2) + ":"
-          + pad(date.getSeconds(), 2) + "."
-          + pad(date.getMilliseconds(), 3)
-      : date.getSeconds() ? "T"
-          + pad(date.getHours(), 2) + ":"
-          + pad(date.getMinutes(), 2) + ":"
-          + pad(date.getSeconds(), 2)
-      : date.getMinutes() || date.getHours() ? "T"
-          + pad(date.getHours(), 2) + ":"
-          + pad(date.getMinutes(), 2)
-      : "");
+  return isNaN(date)
+    ? "Invalid Date"
+    : isUTCMidnight(date)
+      ? pad(date.getUTCFullYear(), 4) + "-" + pad(date.getUTCMonth() + 1, 2) + "-" + pad(date.getUTCDate(), 2)
+      : pad(date.getFullYear(), 4) + "-" + pad(date.getMonth() + 1, 2) + "-" + pad(date.getDate(), 2)
+        + "T" + pad(date.getHours(), 2) + ":" + pad(date.getMinutes(), 2)
+        + (date.getMilliseconds() ? ":" + pad(date.getSeconds(), 2) + "." + pad(date.getMilliseconds(), 3)
+          : date.getSeconds() ? ":" + pad(date.getSeconds(), 2)
+          : "");
 }
